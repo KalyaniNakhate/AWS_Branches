2bf0fe264329bb3e266f071db9694367c4012458
KalyaniNakhate <kalyani.nakhate@gmail.com>
2023-07-08 22:23:46 -0400

    Adding in login_creat branch

---
+++ list.py
@@ -0,0 +1,10 @@
+lis = [10, 20.3, 'python', 'java', 2.1]
+lis[2] = "HI"
+lis[3] = "1993"
+lis.insert(0, 2)
+lis.insert(1, "Kalyani")
+del lis[3]
+lis.append('hello')
+lis2 = [02, 04, 93]
+lis.extend(lis2)
+print(lis)
